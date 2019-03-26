bfd54824f96709eb7c7b2b87d80eae61d5e6d1dd
Gilad Lev Ari <giladl82>
2019-03-26 13:04:20 +0200

    Revert "Attach debug to Redux devTools"
    This reverts commit a6959e66c4a2f371c4a4f7f5f89e045125e80fe9.

--- src/Hooks/Reducer/store.js
+++ src/Hooks/Reducer/store.js
@@ -28,21 +28,13 @@ export const Provider = ({ children, state, reducer, enableDebug }) => {
 
   useEffect(() => {
     if (enableDebug) {
-      if (
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__.send &&
-        typeof window.__REDUX_DEVTOOLS_EXTENSION__.send === 'function'
-      ) {
-        window.__REDUX_DEVTOOLS_EXTENSION__.send(store.__action, getClearState(store));
-      } else {
-        console.log(
-          `%cAction: %c${JSON.stringify(store.__action)} |  %cState: %c${JSON.stringify(getClearState(store))}`,
-          'font-weight:bold; color: green; font-size: 15px;',
-          'color: black; font-size: 15px;',
-          'font-weight:bold; color: blue; font-size: 15px;',
-          'color: black; font-size: 15px;'
-        );
-      }
+      console.log(
+        `%cAction: %c${JSON.stringify(store.__action)} |  %cState: %c${JSON.stringify(getClearState(store))}`,
+        'font-weight:bold; color: green; font-size: 15px;',
+        'color: black; font-size: 15px;',
+        'font-weight:bold; color: blue; font-size: 15px;',
+        'color: black; font-size: 15px;'
+      );
     }
   }, [store]);
 
@@ -65,7 +57,7 @@ export const useStoreCreator = (initialState, reducer) => {
     initialState,
     reducer
   });
-  const ACTION_TYPE = '';
+  const ACTION_TYPE = '@@INIT_STATE';
   const [store, dispatch] = useReducer(reducer, reducer(initialState, { ACTION_TYPE }));
   const [action, setAction] = useState(ACTION_TYPE);
 
