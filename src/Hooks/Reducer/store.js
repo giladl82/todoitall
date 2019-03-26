0bfdb7f680e05bcf99e77165267df092f1b7774e
Gilad Lev Ari <giladl82>
2019-03-26 14:18:11 +0200

    Fixing connection to devTools

--- src/Hooks/Reducer/store.js
+++ src/Hooks/Reducer/store.js
@@ -24,16 +24,23 @@ const Context = React.createContext();
 
 export const Provider = ({ children, state, reducer, enableDebug }) => {
   const store = useStoreCreator(state, reducer, enableDebug);
+  const [devTools, setDevTools] = useState();
   store.__enableDebug = enableDebug;
 
   useEffect(() => {
     if (enableDebug) {
       if (
         window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__.send &&
-        typeof window.__REDUX_DEVTOOLS_EXTENSION__.send === 'function'
+        window.__REDUX_DEVTOOLS_EXTENSION__.connect &&
+        typeof window.__REDUX_DEVTOOLS_EXTENSION__.connect === 'function'
       ) {
-        window.__REDUX_DEVTOOLS_EXTENSION__.send(store.__action, getClearState(store));
+        if (!devTools) {
+          const tools = window.__REDUX_DEVTOOLS_EXTENSION__.connect();
+          setDevTools(tools);
+          tools.init();
+        } else {
+          devTools.send(store.__action, getClearState(store));
+        }
       } else {
         console.log(
           `%cAction: %c${JSON.stringify(store.__action)} |  %cState: %c${JSON.stringify(getClearState(store))}`,
