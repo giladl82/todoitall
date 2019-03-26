a6959e66c4a2f371c4a4f7f5f89e045125e80fe9
Gilad Lev Ari <giladl82>
2019-03-26 12:59:57 +0200

    Attach debug to Redux devTools

--- src/Hooks/Reducer/store.js
+++ src/Hooks/Reducer/store.js
@@ -28,13 +28,21 @@ export const Provider = ({ children, state, reducer, enableDebug }) => {
 
   useEffect(() => {
     if (enableDebug) {
-      console.log(
-        `%cAction: %c${JSON.stringify(store.__action)} |  %cState: %c${JSON.stringify(getClearState(store))}`,
-        'font-weight:bold; color: green; font-size: 15px;',
-        'color: black; font-size: 15px;',
-        'font-weight:bold; color: blue; font-size: 15px;',
-        'color: black; font-size: 15px;'
-      );
+      if (
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__.send &&
+        typeof window.__REDUX_DEVTOOLS_EXTENSION__.send === 'function'
+      ) {
+        window.__REDUX_DEVTOOLS_EXTENSION__.send(store.__action, getClearState(store));
+      } else {
+        console.log(
+          `%cAction: %c${JSON.stringify(store.__action)} |  %cState: %c${JSON.stringify(getClearState(store))}`,
+          'font-weight:bold; color: green; font-size: 15px;',
+          'color: black; font-size: 15px;',
+          'font-weight:bold; color: blue; font-size: 15px;',
+          'color: black; font-size: 15px;'
+        );
+      }
     }
   }, [store]);
 
@@ -57,7 +65,7 @@ export const useStoreCreator = (initialState, reducer) => {
     initialState,
     reducer
   });
-  const ACTION_TYPE = '@@INIT_STATE';
+  const ACTION_TYPE = '';
   const [store, dispatch] = useReducer(reducer, reducer(initialState, { ACTION_TYPE }));
   const [action, setAction] = useState(ACTION_TYPE);
 
