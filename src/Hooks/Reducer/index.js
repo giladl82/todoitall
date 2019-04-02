07e736d1b317c3b0a5ac698c699692458647f2ee
Gilad Lev-Ari <giladl82@gmail.com>
2019-04-02 08:01:05 +0300

    Updating use-hooks-store version

--- src/Hooks/Reducer/index.js
+++ src/Hooks/Reducer/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Provider } from 'use-hooks-store';
-import reducer, { initialState } from '../../state/reducer';
+import reducer from '../../state/reducer';
 import AddTodo from './AddTodo';
 import TodoList from './TodoList';
 
 const TodoApp = () => {
   return (
-    <Provider state={initialState} reducer={reducer} enableDebug={true}>
+    <Provider reducer={reducer} enableDebug={true}>
       <h1>Todo Hooks Reducer</h1>
       <AddTodo />
       <TodoList />
